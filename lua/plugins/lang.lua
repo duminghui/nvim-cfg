59dee3eaf3bcc2109c55a2dd3d20216f2ff9c19d
dumh <duminghui@users.noreply.github.com>
2025-06-14 15:57:47 +0800

    modify fix deprecated function

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -7,7 +7,8 @@ return {
         taplo = {
           -- Fix 'this document has been excluded' (show this when toml file not in a git repo)
           root_dir = function(fname)
-            return util.root_pattern("*.toml")(fname) or util.find_git_ancestor(fname)
+            local git_ancestor = vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+            return util.root_pattern("*.toml")(fname) or git_ancestor
           end,
         },
       },
