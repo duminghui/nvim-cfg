b14093b2b0cd1aa03766be36a707c19eb28b6602
dumh <duminghui@users.noreply.github.com>
2025-10-27 11:03:41 +0800

    fix LazyVim deprecate func

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,17 +11,33 @@ return {
       },
     },
   },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function()
+  --     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --     -- stylua: ignore
+  --     keys[#keys + 1] = { "gd", "<CMD>Trouble lsp_definitions<CR>", desc = "Goto Definition (Trouble)", has = "definition" }
+  --     keys[#keys + 1] = { "gr", "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)", nowait = true }
+  --     keys[#keys + 1] = { "gI", "<CMD>Trouble lsp_implementations<CR>", desc = "Goto Implementation (Trouble)" }
+  --     keys[#keys + 1] = { "gy", "<CMD>Trouble lsp_type_definitions<CR>", desc = "Goto T[y]pe Definition (Trouble)" }
+  --     keys[#keys + 1] = { "gD", "<CMD>Trouble lsp_declarations<CR>", desc = "Goto Declaration (Trouble)" }
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- stylua: ignore
-      keys[#keys + 1] = { "gd", "<CMD>Trouble lsp_definitions<CR>", desc = "Goto Definition (Trouble)", has = "definition" }
-      keys[#keys + 1] = { "gr", "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)", nowait = true }
-      keys[#keys + 1] = { "gI", "<CMD>Trouble lsp_implementations<CR>", desc = "Goto Implementation (Trouble)" }
-      keys[#keys + 1] = { "gy", "<CMD>Trouble lsp_type_definitions<CR>", desc = "Goto T[y]pe Definition (Trouble)" }
-      keys[#keys + 1] = { "gD", "<CMD>Trouble lsp_declarations<CR>", desc = "Goto Declaration (Trouble)" }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "gd", "<CMD>Trouble lsp_definitions<CR>", desc = "Goto Definition (Trouble)", has = "definition" },
+            { "gr", "<CMD>Trouble lsp_references<CR>", desc = "References (Trouble)", nowait = true },
+            { "gI", "<CMD>Trouble lsp_implementations<CR>", desc = "Goto Implementation (Trouble)" },
+            { "gy", "<CMD>Trouble lsp_type_definitions<CR>", desc = "Goto T[y]pe Definition (Trouble)" },
+            { "gD", "<CMD>Trouble lsp_declarations<CR>", desc = "Goto Declaration (Trouble)" },
+          },
+        },
+      },
+    },
   },
   {
     "mason-org/mason.nvim",
